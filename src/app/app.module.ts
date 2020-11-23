25cccd3e4a2f880013c9fefb186fd630eb23b45d
Sean Hammon <git@julysean.com>
2020-11-23 13:48:02 -0700

    Add some basic layout components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideMenuComponent } from './layout/side-menu/side-menu.component';
+import { ContentComponent } from './layout/content/content.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ContainerComponent } from './layout/container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideMenuComponent,
+    ContentComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule
